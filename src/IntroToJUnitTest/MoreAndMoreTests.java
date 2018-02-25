2f016a3d8a8f3ee6f8070568240114564ffed193
jtp5 <league@WTS22.attlocal.net>
2018-02-25 14:29:24 -0800

    2/25/18

--- src/IntroToJUnitTest/MoreAndMoreTests.java
+++ src/IntroToJUnitTest/MoreAndMoreTests.java
@@ -1,6 +1,7 @@
 package IntroToJUnitTest;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -10,14 +11,14 @@ import org.junit.Test;
 
 public class MoreAndMoreTests {
 
-	@Test
-	public void MultTest() {
-
-		assertEquals("10 x 0 = 0", multiply(10, 0));
-		assertEquals("10 x 10 = 100", multiply(10, 10));
-		assertEquals("8 x 11 = 88", multiply(8, 11));
-	}
+	// @Test
+	// public void MultTest() {
 	//
+	// assertEquals("10 x 0 = 0", multiply(10, 0));
+	// assertEquals("10 x 10 = 100", multiply(10, 10));
+	// assertEquals("8 x 11 = 88", multiply(8, 11));
+	// }
+
 	// @Test
 	// public void PrimeTest() {
 	//
@@ -43,22 +44,45 @@ public class MoreAndMoreTests {
 	// assertFalse(isSquare(143));
 	//
 	// }
-	//
-	// @Test
-	// public void CubeTest() {
-	//
-	// assertTrue(isCube(27));
-	// assertTrue(isCube(216));
-	// assertTrue(isCube(729));
-	// assertTrue(isCube(1));
-	// assertFalse(isCube(3));
-	// assertFalse(isCube(22));
-	// assertFalse(isCube(143));
-	//
-	// }
+
+	@Test
+	public void CubeTest() {
+
+		assertTrue(isCube(27));
+		assertTrue(isCube(216));
+		assertTrue(isCube(729));
+		assertTrue(isCube(1));
+		assertFalse(isCube(3));
+		assertFalse(isCube(22));
+		assertFalse(isCube(143));
+
+	}
 
 	public static String multiply(int p1, int p2) {
 		return p1 + " x " + p2 + " = " + (p1 * p2);
+	}
+
+	public static boolean isPrime(int num) {
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static boolean isSquare(int num) {
+		if ((int) (Math.sqrt(num)) * (int) (Math.sqrt(num)) == num) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean isCube(int num) {
+		if ((int) (Math.cbrt(num)) * (int) (Math.cbrt(num)) * (int) (Math.cbrt(num)) == num) {
+			return true;
+		}
+		return false;
 	}
 
 }
--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -1,11 +1,83 @@
 package SearchingAndSortingAlgorithms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0; //<- this needs changing
+		int track = 0;
+		for (String word : eggs) {
+			if (word.equals("cracked")) {
+				return track;
+			}
+			track++;
+		}
+		return -1;
 	}
-	
-	//Add other methods here
+
+	public static int countPearls(List<Boolean> clams) {
+		int count = 0;
+		for (Boolean b : clams) {
+			if (b == true) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static double findTallest(List<Double> heights) {
+		double hold = 0.0;
+		for (double d : heights) {
+			if (d > hold) {
+				hold = d;
+			}
+		}
+		return hold;
+	}
+
+	public static String findLongestWord(List<String> words) {
+		String output = "";
+		for (String s : words) {
+			if (s.length() > output.length()) {
+				output = s;
+			}
+		}
+		return output;
+	}
+
+	public static boolean containsSOS(List<String> morse) {
+		for (String s : morse) {
+			if (s.equals(" ... --- ... ")) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static ArrayList<Double> sortScores(List<Double> scores) {
+		ArrayList<Double> alist = new ArrayList<Double>();
+		boolean swap = false;
+		double holder = 0;
+		for (double d : scores) {
+			alist.add(d);
+		}
+		for (int i = 1; i < alist.size(); i++) {
+			if (alist.get(i - 1) > alist.get(i)) {
+				holder = alist.get(i - 1);
+				alist.set(i - 1, alist.get(i));
+				alist.set(i, holder);
+				swap = true;
+			}
+		}
+		List<Double> newList = Arrays.asList(new Double[alist.size()]);
+		for (int i = 0; i < alist.size(); i++) {
+			newList.add(i, alist.get(i));
+		}
+		if (swap == true) {
+			sortScores(newList);
+		}
+		return alist;
+	}
+
 }
